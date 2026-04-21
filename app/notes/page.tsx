6c05065b3884743e59f046ed69ba535d3e4ed814
I.Kumpan <ilyakumpan11@gmail.com>
2026-04-21 20:26:24 +0300

    mentor-02

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -4,21 +4,23 @@ import {
   QueryClient,
 } from '@tanstack/react-query';
 import { fetchNotes } from '@/lib/api';
-import dynamic from 'next/dynamic';
+import dynamic from 'next/dynamic'; // Додаємо імпорт dynamic
+
 
 const NotesClient = dynamic(() => import('./Notes.client'), {
   ssr: false,
 });
+
 export default async function NotesPage() {
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
     queryKey: ['notes'],
-    queryFn: () => fetchNotes({ page: 1, perPage: 10 }), 
+    queryFn: () => fetchNotes({ page: 1, perPage: 10 }),
   });
 
   return (
-    <main> 
+    <main>
       <HydrationBoundary state={dehydrate(queryClient)}>
         <NotesClient />
       </HydrationBoundary>
